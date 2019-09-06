3fd3557db42bb5024e13908da3cacd1820f60d56
Jorge González <iksaku@me.com>
2019-09-06 12:15:13 -0500

    Try to auth first, if success, continue with command

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-sh -c "swift $*" 2>/dev/null
+swift auth > /dev/null 2>&1
 
-if [ $? -ne 0 ]; then
+if [[ $? -ne 0 ]]; then
     echo "[Error] Unable to authenticate against Swift provider."
     echo -e "\tPlease ensure all your keystone variables are set and check your credentials."
     exit 1
 fi
+
+sh -c "swift $*"
